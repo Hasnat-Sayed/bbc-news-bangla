25cfc8adcd9ec8a79ad1c2a5aa1fdcf972530cdd
Hasnat-Sayed <hasnatbin1@gmail.com>
2025-09-15 02:03:01 +0600

    bookmark and modal added

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const categoryContainer = document.getElementById("categoryContainer");
 const newsContainer = document.getElementById("newsContainer");
+const bookmarkContainer = document.getElementById("bookmarkContainer");
+const bookmarkCount = document.getElementById('bookmarkCount')
+const newsDetailsModal = document.getElementById('news-details-modal')
+const modalContainer = document.getElementById('modalContainer')
+
+
+let bookmarks = [];
 
 //load categories
 const loadCategory = () => {
@@ -36,7 +43,7 @@ const showCategory = (categories) => {
       });
 
 
-      //console.log(e.target)
+      showLoading()
       e.target.classList.add("border-b-4");
       loadNewsByCategory(e.target.id);
     }
@@ -60,11 +67,16 @@ const loadNewsByCategory = (categoryId) => {
 //show news
 const showNewsByCategory = (articles) => {
 
+  if (articles.length === 0) {
+    showEmptyMessage()
+    
+    return
+  }
   newsContainer.innerHTML = "";
   articles.forEach((article) => {
     newsContainer.innerHTML += `
-        <div class="border  border-gray-300 rounded-b-lg  flex flex-col" >
-            <div>
+        <div class="border  border-gray-300 rounded-lg overflow-hidden flex flex-col" >
+            <div >
              <img src="${article.image.srcset[5].url}"/>
             </div>
             <div id="${article.id}" class="p-2 flex flex-col flex-grow">
@@ -80,6 +92,120 @@ const showNewsByCategory = (articles) => {
         `;
   });
 };
+
+
+//buttons clicked
+newsContainer.addEventListener("click", (e) => {
+  if (e.target.innerText === "Bookmark") {
+    handleBookmarks(e);
+  }
+
+  if (e.target.innerText === "View Details") {
+    handleViewDetails(e)
+  }
+});
+
+//bookmark handle
+const handleBookmarks = (e) => {
+  const title = e.target.parentNode.parentNode.children[0].innerText;
+  const id = e.target.parentNode.parentNode.id;
+  const alreadyExists = bookmarks.find((book) => book.id === id)
+  
+  if(!alreadyExists){
+    bookmarks.push({
+    title: title,
+    id: id,
+  });
+  } 
+  
+  showBookmarks(bookmarks);  
+};
+
+//show bookmark
+const showBookmarks = (bookmarks) => {
+    bookmarkContainer.innerHTML = ""
+    bookmarks.forEach(bookmark => {
+        bookmarkContainer.innerHTML += `
+        <div class="border-y-gray-300 border border-transparent my-2 p-2">
+            <h1>${bookmark.title}</h1>
+            <button onclick="handleDeleteBookmark('${bookmark.id}')" class="btn btn-sm btn-outline btn-error">Delete</button>
+        </div>
+        `
+    })
+
+    bookmarkCount.innerText = bookmarks.length
+};
+
+//delete button
+const handleDeleteBookmark = (bookmarkId) => {
+   const filteredBookmarks =  bookmarks.filter(bookmark => bookmark.id !== bookmarkId)
+   bookmarks = filteredBookmarks
+   showBookmarks(bookmarks)
+
+}
+
+//view details button
+handleViewDetails = (e) => {
+const id = e.target.parentNode.parentNode.id;
+
+  
+  fetch(`https://news-api-fs.vercel.app/api/news/${id}`)
+  .then(res=> res.json())
+  .then(data => {
+    
+    showDetailsNews(data.article)
+
+  })
+  .catch(err => {
+    newsDetailsModal.showModal()
+    modalContainer.innerHTML = ""
+    modalContainer.innerHTML = `
+      <h1 class= "text-xl font-bold">No details for this news now</h1>         
+    `
+  })
+
+}
+
+//modal
+const showDetailsNews = (article) => {
+  console.log(article)
+    newsDetailsModal.showModal()
+    modalContainer.innerHTML = ""
+    modalContainer.innerHTML = `
+      <h1 class= "text-xl font-bold">${article.title}</h1>
+      <img src="${article.images[0].url}"/>
+      <p class= "text-sm font-bold">${article.timestamp}</p>
+      <p>${article.content.join("")}</p>
+    `
+}
+
+
+//loading function
+const showLoading = () => {
+    newsContainer.innerHTML = `
+    <div class="col-span-3 flex justify-center items-center min-h-[70px]">
+      <div class="bg-red-500 p-3 loading loading-spinner loading-xl text-center"></div>
+    </div>
+    `
+}
+
+//error handle
+const showError = () => {
+    newsContainer.innerHTML = `
+    <div class="col-span-3 flex justify-center items-center min-h-[70px]">
+      <div class="bg-red-500 p-6 text-white rounded-lg text-center">Something Went Wrong</div>
+    </div>
+    `
+}
+
+//empty news
+const showEmptyMessage = () => {
+    newsContainer.innerHTML = `
+    <div class="col-span-3 flex justify-center items-center min-h-[70px]">
+      <div class="bg-orange-500 p-6 text-white rounded-lg text-center">No news found for this category</div>
+    </div>
+    `
+}
 
 loadCategory();
 loadNewsByCategory("main")
