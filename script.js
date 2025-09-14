02ced41d8fb9d012427727a0d3ec446db1f0ea76
Hasnat-Sayed <hasnatbin1@gmail.com>
2025-09-14 23:14:10 +0600

    news added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const categoryContainer = document.getElementById("categoryContainer");
-
+const newsContainer = document.getElementById("newsContainer");
 
 //load categories
 const loadCategory = () => {
@@ -18,24 +18,69 @@ const loadCategory = () => {
 const showCategory = (categories) => {
   categories.forEach((cat) => {
     categoryContainer.innerHTML += `
-      <li id="${cat.id}" class="border-transparent border-b-4 py-1 border-red-600 hover:border-red-600 cursor-pointer">${cat.title}</li>
+      <li id="${cat.id}" class="py-1 hover:border-b-4 hover:border-red-600 border-red-600 cursor-pointer">${cat.title}</li>
       `;
   });
 
-  //active border
-  categoryContainer.addEventListener("click", (e) => {
-    const allLi = document.querySelectorAll("li");
-    allLi.forEach((li) => {
-      li.classList.remove("border-b-4");
-    });
+  //adds active state to main page 
+  const mainPage = document.getElementById("main");
+  mainPage.classList.add("border-b-4");
+  
 
-    if (e.target.localName === "li") {;
-      showLoading()
+  //category clicked
+  categoryContainer.addEventListener("click", (e) => {
+    if (e.target.localName === "li") {
+      const allLi = document.querySelectorAll("li");
+      allLi.forEach((li) => {
+        li.classList.remove("border-b-4");
+      });
+
+
+      //console.log(e.target)
       e.target.classList.add("border-b-4");
       loadNewsByCategory(e.target.id);
     }
   });
 };
 
+
+//news load
+const loadNewsByCategory = (categoryId) => {
+  
+  fetch(`https://news-api-fs.vercel.app/api/categories/${categoryId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      showNewsByCategory(data.articles);
+    })
+    .catch((err) => {
+      showError()
+    });
+};
+
+//show news
+const showNewsByCategory = (articles) => {
+
+  newsContainer.innerHTML = "";
+  articles.forEach((article) => {
+    newsContainer.innerHTML += `
+        <div class="border  border-gray-300 rounded-b-lg  flex flex-col" >
+            <div>
+             <img src="${article.image.srcset[5].url}"/>
+            </div>
+            <div id="${article.id}" class="p-2 flex flex-col flex-grow">
+              <h1 class="font-extrabold">${article.title}</h1>
+              <p class="text-sm">${article.time}</p>
+              <div class="mt-auto flex gap-3">
+                <button class="btn btn-outline flex-1">Bookmark</button>
+                <button class="btn btn-outline  flex-1">View Details</button>
+              </div>
+                
+            </div>
+        </div>
+        `;
+  });
+};
+
 loadCategory();
+loadNewsByCategory("main")
 
